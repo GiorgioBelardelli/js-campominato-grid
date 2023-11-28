f9ef35e08c126762557cdfdbf0737c70e3988a22
GiorgioBelardelli <asterapocalypse@gmail.com>
2023-11-28 17:05:19 +0100

    feat: added difficulties

--- BONUS/JS/script.js
+++ BONUS/JS/script.js
@@ -1,6 +1,6 @@
 const gridElement = document.getElementById("grid");
 const playButton = document.getElementById("play-button");
-const selectDiff = document.getElementById("select-difficolta");
+
 
 
 
@@ -9,9 +9,12 @@ playButton.addEventListener ("click",
     function(){
 
         gridElement.innerHTML = "";
+        let selectDiff = parseInt(document.getElementById("select-difficolta").value);
 
+        console.log(selectDiff);
 
-            for( let i=1; i <= 100; i++ ){
+        if(selectDiff===0){
+          for( let i=1; i <= 100; i++ ){
                 
                 const newSquare = createSquare("div", "square"); 
                 newSquare.append(i);
@@ -24,7 +27,41 @@ playButton.addEventListener ("click",
                         console.log(i)
                     }
                 );
+            }  
+        }else if(selectDiff===1){
+            for( let i=1; i <= 81; i++ ){
+                
+                const newSquare = createSquare("div", "square"); 
+                newSquare.classList.add("squareMedium");
+                newSquare.append(i);
+                gridElement.append(newSquare);
+
+                newSquare.addEventListener("click", 
+                        
+                    function() {
+                        this.classList.add("clicked")
+                        console.log(i)
+                    }
+                );
             }
+        }else if (selectDiff===2){
+            for( let i=1; i <= 49; i++ ){
+                
+                const newSquare = createSquare("div", "square");
+                newSquare.classList.add("sqaureHard"); 
+                newSquare.append(i);
+                gridElement.append(newSquare);
+
+                newSquare.addEventListener("click", 
+                        
+                    function() {
+                        this.classList.add("clicked")
+                        console.log(i)
+                    }
+                );
+            }
+        }
+            
     }
 )
 
